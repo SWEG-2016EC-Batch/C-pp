ac9f9c9ec3a616511e24c257af340a9305745961
Mahder Ashenafi <mahder.ashenafi@aastustudent.edu.et>
2024-12-02 22:04:28 +0300

    Update mile_calculator.cpp

--- ETS0881_Mahder_Ashenafi/mile_calculator.cpp
+++ ETS0881_Mahder_Ashenafi/mile_calculator.cpp
@@ -1,57 +1,30 @@
 #include <iostream>
 using namespace std;
+
 int main()
 {
-    int cont = 1;
-    while (cont == 1)
+    double gallon_gas, mile_gallon, total_mile;
+    cout << "Enter the gas capacity of your car in gallons = ";
+    cin >> gallon_gas;
+
+    if (cin.fail() || gallon_gas < 0)
     {
-        float capacity, miles_per_gallon, miles;
-        int tryal = 0;
-
-    a:
-        cout << "Enter capacity(gallons) : ";
-        cin >> capacity;
-
-        if (cin.fail())
-        {
-            cin.clear();
-            tryal += 1;
-            if (tryal < 3)
-            {
-                cout << "Please enter a valid input !" << endl;
-                goto a;
-            }
-            else
-            {
-                exit(0);
-            }
-        }
-
-        b: 
-        cout << "Enter miles per gallon of fuel : ";
-        cin >> miles_per_gallon;
-
-        if (cin.fail())
-        {
-            cin.clear();
-            tryal += 1;
-            if (tryal < 3)
-            {
-                cout << "Please enter a valid input !" << endl;
-                goto b;
-            }
-            else
-            {
-                exit(0);
-            }
-        }
-
-        miles = capacity * miles_per_gallon;
-
-        cout << "Number of miles that can be driven : " << miles << endl;
-
-        cout << "Do you want to continue? Yes(1) or No(0) : ";
-        cin >> cont;
+        cout << "Invalid input! Fuel tank capacity can not be a negative number and non numeric character.";
+        return 0;
     }
+
+    cout << "Enter the mile your Automobile can go per gallon = ";
+    cin >> mile_gallon;
+
+    if (cin.fail() || mile_gallon < 0)
+    {
+        cout << "Invalid input! Fuel tank capacity can not be a negative number and non numeric character.";
+        return 0;
+    }
+
+    total_mile = mile_gallon * gallon_gas;
+
+    cout << "Your Automobile is capable of driving " << total_mile << " miles with a full gas without refueling." << endl;
+
     return 0;
 }

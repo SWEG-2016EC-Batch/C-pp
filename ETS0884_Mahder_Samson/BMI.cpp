743005ec5d1cae003922b23cf29878135120cbdd
Mahder Samson Kebede <mahder.samson@aastustudent.edu.et>
2024-12-02 11:54:22 +0300

    Update BMI.cpp

--- ETS0884_Mahder_Samson/BMI.cpp
+++ ETS0884_Mahder_Samson/BMI.cpp
@@ -1,37 +1,37 @@
 #include <iostream>
 using namespace std;
 int main(){
-    int cont=1;
-    float height, weight, BMI;
-   while(cont==1){
-cout<<"Enter your height(m): "<<endl;
+ 
+    float BMI, height,weight;
+    int continue_=1;
+    while(continue_==1){
+    cout<<"please enter your height";
     cin>>height;
-     if(cin.fail()||height<=0){
-        cout<<"please enter a valid input!"<<endl;
-        cin.clear(); 
-            cin.ignore();
-            continue;}
-     cout<<"Enter your weight(kg): "<< endl;
+    cout<<"height=  "<<height<<endl;
+    cout<<"please enter your weight";
     cin>>weight;
-     if(cin.fail()||weight<=0){
-        cout<<"please enter a valid input!"<<endl;
-        cin.clear(); 
-            cin.ignore();
-            continue;}
-  
-    BMI=weight/(height*height);
-    cout << "Your body mass index is: "<< BMI<<endl;
-    if (BMI<18.5){
-cout <<"Under weight"<<endl;
-
-    }else if(BMI>=18.5 && BMI<24.9){
-        cout <<"Normal weight"<<endl;
-    }else{
-        cout<<" Over weight"<<endl;
-    }
-     cout << "Do you want to continue? Yes(1) to continue or No(0) to stop : ";
-        cin >> cont;
-   }
+     cout<<"weight=  "<<weight<<endl;
+     BMI=weight/(height*height);
+     cout<<"your body mass index BMI= "<<BMI<<endl;
+     if (BMI<18.5){
+        cout<<"status= under weight"<<endl;
+        }
+        else if (18.5 <= BMI <= 24.9){
+        cout<<"status= normal weight"<<endl;
+        } else if (25<=BMI<=29.9){
+            cout<<"status= over weight"<<endl;
+        }
+        else if(BMI>=30){
+            cout<<"obesity"<<endl;
     
+        } else {
+     break;
+        } 
+        cout<<"do you want to continue? enter 1 if yes, otherwise enter any number except 1."<<endl;
+        cin>>continue_;
+        
+
+    }
+   
     return 0;
-}
+    }

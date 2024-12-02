4044a9be9748bfbc73465ef3cba4c66534a888bf
Mahder Ashenafi <mahder.ashenafi@aastustudent.edu.et>
2024-12-02 11:44:55 +0300

    Update mile_calculator.cpp

--- ETS0881_Mahder_Ashenafi/mile_calculator.cpp
+++ ETS0881_Mahder_Ashenafi/mile_calculator.cpp
@@ -6,12 +6,45 @@ int main()
     while (cont == 1)
     {
         float capacity, miles_per_gallon, miles;
+        int tryal = 0;
 
+    a:
         cout << "Enter capacity(gallons) : ";
         cin >> capacity;
 
+        if (cin.fail())
+        {
+            cin.clear();
+            tryal += 1;
+            if (tryal < 3)
+            {
+                cout << "Please enter a valid input !" << endl;
+                goto a;
+            }
+            else
+            {
+                exit(0);
+            }
+        }
+
+        b: 
         cout << "Enter miles per gallon of fuel : ";
         cin >> miles_per_gallon;
+
+        if (cin.fail())
+        {
+            cin.clear();
+            tryal += 1;
+            if (tryal < 3)
+            {
+                cout << "Please enter a valid input !" << endl;
+                goto b;
+            }
+            else
+            {
+                exit(0);
+            }
+        }
 
         miles = capacity * miles_per_gallon;
 

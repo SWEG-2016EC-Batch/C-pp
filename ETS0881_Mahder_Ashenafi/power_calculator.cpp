9f411d6385907f9226bcd9d5066b186ed36b8a76
Mahder Ashenafi <mahder.ashenafi@aastustudent.edu.et>
2024-12-02 21:27:34 +0300

    Update power_calculator.cpp

--- ETS0881_Mahder_Ashenafi/power_calculator.cpp
+++ ETS0881_Mahder_Ashenafi/power_calculator.cpp
@@ -2,19 +2,78 @@
 using namespace std;
 int main()
 {
-    int x, y, product;
+    float x, product;
+    int y;
+    int tryal = 0;
     product = 1;
 
+a:
     cout << "Enter x: ";
     cin >> x;
 
+    if (cin.fail())
+    {
+        cin.clear();
+        tryal += 1;
+        if (tryal <= 2)
+        {
+            cout << "Enter a valid value you have "
+                 << 3 - tryal << " tries left." << endl;
+            goto a;
+        }
+        else
+        {
+            exit(0);
+        }
+    }
+
+b:
     cout << "Enter y: ";
     cin >> y;
 
-    for (int i = 0; i < y;i++)
+    if (cin.fail())
     {
-        product *= x;
+        cin.clear();
+        tryal += 1;
+        if (tryal <= 2)
+        {
+            cout << "Enter a valid value you have "
+                 << 3 - tryal << " tries left." << endl;
+            goto b;
+        }
+        else
+        {
+            exit(0);
+        }
     }
 
-    cout << x <<"^"<< y << " is " <<product << endl;
+    if (x == 0)
+    {
+        if (y <= 0)
+        {
+            cout << "Undefined" << endl;
+            exit(0);
+        }
+        cout << "0 ^ " << y << " is 0" << endl;
+    }
+    if (y > 0)
+    {
+        for (int i = 0; i < y; i++)
+        {
+            product *= x;
+        }
+    }
+    else if (y < 0)
+    {
+        for (int i = 0; i > y; i--)
+        {
+            product *= 1 / x;
+        }
+    }
+    else
+    {
+        product = 1;
+    }
+
+    cout << x << "^" << y << " is " << product << endl;
 }

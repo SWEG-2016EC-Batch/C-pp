569d98f1f26bfb3c80a565d1fb8abc1aff711ae9
Meaza Zelalem Gelaw <meaza.zelalem@aastustudent.edu.et>
2024-12-13 06:14:26 -0800

    Update Net_salary.cpp

--- ETS0907_Meaza_Zelalem/assign_2/Net_salary.cpp
+++ ETS0907_Meaza_Zelalem/assign_2/Net_salary.cpp
@@ -1,57 +1,53 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    double basic_salary, net_pay, pension_deduction, bonus = 0, gross_salary, bonus_rate, tax_deduction = 0, tax_rate = 0;
-    int worked_hour;
-
-   
-    cout << "Enter your basic salary: ";
+int main()
+{
+    float net_pay, pension, tax, gross_salary, bonus, tax_rate, worked_hours, bonus_rate;
+    int basic_salary;
+    float extra_hour = 0;
+    
+    cout << "Enter your basic salary = ";
     cin >> basic_salary;
-    cout << "Enter your total worked hours: ";
-    cin >> worked_hour;
+    cout << "Enter your worked hours = ";
+    cin >> worked_hours;
+    cout << "Enter bonus rate = ";
+    cin >> bonus_rate;
 
- 
-    pension_deduction = basic_salary * 0.07;
-
-    
-    if (worked_hour > 40) {
-        cout << "Enter bonus rate per extra hour: ";
-        cin >> bonus_rate;
-        bonus = (worked_hour - 40) * bonus_rate;
+    if (worked_hours > 40)
+    {
+        cout << "Enter over time bonus hour = ";
+        cin >> extra_hour;
     }
-
     
+
+    bonus = extra_hour * bonus_rate;
     gross_salary = basic_salary + bonus;
+    pension = basic_salary * 0.07;
 
-
-    if (basic_salary >= 0 && basic_salary < 200) {
-        tax_rate = 0;
-    } else if (basic_salary >= 200 && basic_salary < 600) {
-        tax_rate = 0.1;
-    } else if (basic_salary >= 600 && basic_salary < 1200) {
-        tax_rate = 0.15;
-    } else if (basic_salary >= 1200 && basic_salary < 2000) {
+    if (basic_salary <= 200) {
+       tax_rate = 0;
+    } else if (basic_salary <= 600) {
+      tax_rate = 0.1;
+    } else if (basic_salary <= 1200) {
+    tax_rate = 0.15;
+    } else if (basic_salary <= 2000) {
         tax_rate = 0.2;
-    } else if (basic_salary >= 2000 && basic_salary < 3500) {
-        tax_rate = 0.25;
-    } else if (basic_salary >= 3500) {
-        tax_rate = 0.3;
+    } else if (basic_salary <= 3500) {
+       tax_rate = 0.25;
     } else {
-        cout << "Invalid salary amount!" << endl;
-        return 0;
+      tax_rate = 0.3;
     }
+    tax = gross_salary * tax_rate;
+    net_pay = gross_salary - pension - tax;
 
-
-    tax_deduction = gross_salary * tax_rate;
-
-    net_pay = gross_salary - pension_deduction - tax_deduction;
-
- 
-    cout << "Pension deduction: " << pension_deduction << endl;
-    cout << "Gross salary: " << gross_salary << endl;
-    cout << "Tax deduction: " << tax_deduction << endl;
-    cout << "Net pay: " << net_pay << endl;
+    cout <<'\n';
+    cout << "Bonus = " << bonus <<'\n';
+    cout << "Gross salary = " << gross_salary <<'\n';
+    cout << "Tax = " << tax <<'\n';
+    cout << "Pension = " << pension <<'\n';
+    cout << "Net salary = " << net_pay <<'\n';
+    cout <<'\n';
 
     return 0;
 }
